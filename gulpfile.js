18d652610725f358be2d47851ea3e51973e530e9
Bartek <siwek.b@gmail.com>
2018-01-08 23:22:55 +0100

    corect gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,3 @@
-// var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// var sourcemaps = require('gulp-sourcemaps');
-// var browserSync = require('browser-sync').create();
-//
-// gulp.task('browserSync', function() {
-//   var files = [
-//     '*.html',
-//     'css/*.css'
-//   ];
-//     browserSync.init(files, {
-//         server: {
-//           baseDir: './'
-//         }
-//     });
-// });
-//
-// gulp.task('sass', function() {
-// return gulp.src('scss/**/*.scss')
-// .pipe(sourcemaps.init())
-// .pipe(sass({outputStyle: 'nested', errLogToConsole: true}))
-// .pipe(sourcemaps.write())
-// .pipe(gulp.dest('css'))
-// })
-// gulp.task('watch', function(){
-// gulp.watch('scss/**/*.scss', ['sass']);
-// })
-//
-// gulp.task('default', ['watch', 'browserSync'], function () {
-//
-// });
-
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
